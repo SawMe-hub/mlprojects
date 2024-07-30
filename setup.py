7d0af562b9aefd1fd6da6cbc18a03bbc88effd44
SawMe-hub <kurkuteswami2003@gmail.com>
2024-07-30 11:21:34 +0530

    Your descriptive commit message

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+setup(
+
+    name='mlprojects',
+    version='0.0.1',
+    author='Saw_me',
+    author_email='kurkuteswami2003@gmail.com'
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+    
+
+)
